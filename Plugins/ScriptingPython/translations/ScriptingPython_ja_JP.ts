7bbf251dd27576fb6e396836fe16d1bb1b2c1d8b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:57:19 +0200

    New translations scriptingpython.ts (Japanese)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_ja_JP.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_ja_JP.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>%1 関数の使用が無効です。%2 個の引数が必要ですが、%3 個が指定されました。</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>%1 関数の使用が無効です。%2 個の引数が必要ですが、%3 個が指定されました。</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>関数 %1 から不明なエラーが返されました。</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>関数 %1 から不明なエラーが返されました。</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>関数 %1 に引数として渡された Python オブジェクトの文字列表現を計算できませんでした。</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>関数 %1 に引数として渡された Python オブジェクトの文字列表現を計算できませんでした。</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>関数 %1 の実行コンテキストが見つかりませんでした。これは Python プラグインのバグです。報告してください。</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>関数 %1 の実行コンテキストが見つかりませんでした。これは Python プラグインのバグです。報告してください。</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Python 関数 %1 からのエラー: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Python 関数 %1 からのエラー: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Python ライブラリのパス</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Python ライブラリのパス</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Python ライブラリファイルを選択</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Python ライブラリファイルを選択</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>プラグインが正しく設定されていません。</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>プラグインが正しく設定されていません。</translation>
     </message>
-</context>
+  </context>
 </TS>
