14cac471e7b049a9fcda7e1e02f279d97c31fdb4
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:51:38 +0200

    New translations letos.ts (Romanian)
    [ci skip]

--- Letos/letos/translations/letos_ro_RO.ts
+++ Letos/letos/translations/letos_ro_RO.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ro" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>Interfață grafică pentru Letos, un manager SQLite.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>Interfață grafică pentru Letos, un manager SQLite.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>Pornește aplicația în modul sigur fără a restaura sesiunea anterioară. Utilizați această opțiune pentru a evita problemele cauzate de o sesiune coruptă.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>Pornește aplicația în modul sigur fără a restaura sesiunea anterioară. Utilizați această opțiune pentru a evita problemele cauzate de o sesiune coruptă.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>Nu încarcă niciun plugin. Poate fi utilizată împreună cu modul sigur pentru a izola suplimentar eventualele probleme.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>Nu încarcă niciun plugin. Poate fi utilizată împreună cu modul sigur pentru a izola suplimentar eventualele probleme.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>Activează mesajele de debug în consolă (accesibilă cu F12).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>Activează mesajele de debug în consolă (accesibilă cu F12).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>Redirecționează mesajele de debug către ieșirea standard (forțează modul debug).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>Redirecționează mesajele de debug către ieșirea standard (forțează modul debug).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>Redirecționează mesajele de debug către fișierul indicat (forțează modul debug).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>Redirecționează mesajele de debug către fișierul indicat (forțează modul debug).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>fișier jurnal</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>fișier jurnal</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>Activează mesajele de debug ale parserului Lemon pentru asistentul de cod SQL.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>Activează mesajele de debug ale parserului Lemon pentru asistentul de cod SQL.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>Activează debug-ul pentru fiecare interogare SQL trimisă către orice bază de date.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>Activează debug-ul pentru fiecare interogare SQL trimisă către orice bază de date.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>Limitează mesajele interogărilor SQL numai la &lt;baza de date&gt; indicată.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>Limitează mesajele interogărilor SQL numai la &lt;baza de date&gt; indicată.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>bază de date</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>bază de date</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>Indică fișierul principal de configurare. Citiți manualul de pe pagina wiki pentru mai multe detalii.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>Indică fișierul principal de configurare. Citiți manualul de pe pagina wiki pentru mai multe detalii.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>fișier de setări</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>fișier de setări</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Activează depanarea executorului de interogări al Letos&apos;.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Activează depanarea executorului de interogări al Letos&apos;.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Listează pluginurile instalate în Letos și se închide.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Listează pluginurile instalate în Letos și se închide.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>fișier</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>fișier</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>Fișierul bazei de date de deschis</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>Fișierul bazei de date de deschis</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>Selectați directorul de configurare</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>Selectați directorul de configurare</translation>
     </message>
-</context>
+  </context>
 </TS>
