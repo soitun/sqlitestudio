a5ccfab8f3ff8c5eb7fbdc1e2cac9121c4c3b0d7
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:19:16 +0200

    New translations sqlformattersimple.ts (Korean)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_ko_KR.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_ko_KR.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ko" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>키워드를 대문자로 변환</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>키워드를 대문자로 변환</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>여러 공백 문자를 단일 공백으로 줄이기</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>여러 공백 문자를 단일 공백으로 줄이기</translation>
     </message>
-</context>
+  </context>
 </TS>
