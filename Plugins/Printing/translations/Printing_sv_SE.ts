78d33c70094973bf2f124f26a691e34b9dca4927
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:16:16 +0200

    New translations printing.ts (Swedish)
    [ci skip]

--- Plugins/Printing/translations/Printing_sv_SE.ts
+++ Plugins/Printing/translations/Printing_sv_SE.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv-SE" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="35"/>
-        <source>Print data</source>
-        <translation>Skriv ut data</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>Skriv ut data</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="37"/>
-        <source>Print query</source>
-        <translation>Skriv ut fråga</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>Skriv ut fråga</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="74"/>
-        <source>No data to print.</source>
-        <translation>Inga data att skriva ut.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>Inga data att skriva ut.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="82"/>
-        <source>Printing data.</source>
-        <translation>Skriver ut data.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>Skriver ut data.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="108"/>
-        <source>Printing query.</source>
-        <translation>Skriver ut fråga.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>Skriver ut fråga.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>Utskrift</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Utskrift</translation>
     </message>
-</context>
+  </context>
 </TS>
