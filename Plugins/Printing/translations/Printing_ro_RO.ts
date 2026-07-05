99b00cd7d72ef92af9a30033561c54eb70d9567d
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:15:49 +0200

    New translations printing.ts (Romanian)
    [ci skip]

--- Plugins/Printing/translations/Printing_ro_RO.ts
+++ Plugins/Printing/translations/Printing_ro_RO.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ro" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="35"/>
-        <source>Print data</source>
-        <translation>Tipărește datele</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>Tipărește datele</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="37"/>
-        <source>Print query</source>
-        <translation>Tipărește interogarea</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>Tipărește interogarea</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="74"/>
-        <source>No data to print.</source>
-        <translation>Nu există date de tipărit.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>Nu există date de tipărit.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="82"/>
-        <source>Printing data.</source>
-        <translation>Se tipăresc datele.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>Se tipăresc datele.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="108"/>
-        <source>Printing query.</source>
-        <translation>Se tipărește interogarea.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>Se tipărește interogarea.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>Tipărire</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Tipărire</translation>
     </message>
-</context>
+  </context>
 </TS>
