19a5e60a5326f1a62a32680b0d5bee5b7e223f0d
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:54:05 +0200

    New translations dbsqlitemc.ts (Portuguese)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_pt_PT.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_pt_PT.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pt-PT" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Senha (chave)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Senha (chave)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Deixe em branco para criar ou conectar-se a um banco de dados descriptografado.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Deixe em branco para criar ou conectar-se a um banco de dados descriptografado.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Senha de criptografia</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Senha de criptografia</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Cifra</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Cifra</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>A cifra determina o algoritmo de criptografia usado para criptografar o banco de dados.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>A cifra determina o algoritmo de criptografia usado para criptografar o banco de dados.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Configuração da cifra (opcional)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Configuração da cifra (opcional)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>Instruções PRAGMA para personalizar a configuração do SQLite3 Multiple Ciphers, como iterações de KDF, modo legado etc.
+      <translation>Instruções PRAGMA para personalizar a configuração do SQLite3 Multiple Ciphers, como iterações de KDF, modo legado etc.
 Elas serão executadas a cada abertura do banco de dados.
 Consulte a documentação do SQLite3 Multiple Ciphers para obter detalhes.</translation>
     </message>
-</context>
+  </context>
 </TS>
