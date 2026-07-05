3a7e90a9cade87b00f6c8c7d971b1262f3e87c1a
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:01 +0200

    New translations sqlenterpriseformatter.ts (Serbian (Cyrillic))
    [ci skip]

--- Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_sr_SP.ts
+++ Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_sr_SP.ts
@@ -1,228 +1,228 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sr" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../sqlenterpriseformatter.cpp" line="89"/>
-        <location filename="../sqlenterpriseformatter.cpp" line="96"/>
-        <source>name</source>
-        <comment>example name wrapper</comment>
-        <translation>име</translation>
+      <location filename="../sqlenterpriseformatter.cpp" line="89"/>
+      <location filename="../sqlenterpriseformatter.cpp" line="96"/>
+      <source>name</source>
+      <comment>example name wrapper</comment>
+      <translation>име</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SqlEnterpriseFormatter</name>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="46"/>
-        <source>Indentation</source>
-        <translation>Увлачење</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="46"/>
+      <source>Indentation</source>
+      <translation>Увлачење</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="58"/>
-        <source>Line up keywords in multi-line queries</source>
-        <translation>Поравнај кључне речи у вишелинијским упитима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="58"/>
+      <source>Line up keywords in multi-line queries</source>
+      <translation>Поравнај кључне речи у вишелинијским упитима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="68"/>
-        <source>Indent contents of parenthesis block</source>
-        <translation>Увуци садржај блока заграда</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="68"/>
+      <source>Indent contents of parenthesis block</source>
+      <translation>Увуци садржај блока заграда</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="91"/>
-        <source>Tab size:</source>
-        <translation>Величина табулатора:</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="91"/>
+      <source>Tab size:</source>
+      <translation>Величина табулатора:</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="106"/>
-        <source>New lines</source>
-        <translation>Нови редови</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="106"/>
+      <source>New lines</source>
+      <translation>Нови редови</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="160"/>
-        <source>Before opening parenthesis in column definitions</source>
-        <translation>Пре отварајуће заграде у дефиницијама колона</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="160"/>
+      <source>Before opening parenthesis in column definitions</source>
+      <translation>Пре отварајуће заграде у дефиницијама колона</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="170"/>
-        <source>After opening parenthesis in column definitions</source>
-        <translation>После отварајуће заграде у дефиницијама колона</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="170"/>
+      <source>After opening parenthesis in column definitions</source>
+      <translation>После отварајуће заграде у дефиницијама колона</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="180"/>
-        <source>Before closing parenthesis in column definitions</source>
-        <translation>Пре затварајуће заграде у дефиницијама колона</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="180"/>
+      <source>Before closing parenthesis in column definitions</source>
+      <translation>Пре затварајуће заграде у дефиницијама колона</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="190"/>
-        <source>After closing parenthesis in column definitions</source>
-        <translation>После затварајуће заграде у дефиницијама колона</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="190"/>
+      <source>After closing parenthesis in column definitions</source>
+      <translation>После затварајуће заграде у дефиницијама колона</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="200"/>
-        <source>Before opening parenthesis in expressions</source>
-        <translation>Пре отварајуће заграде у изразима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="200"/>
+      <source>Before opening parenthesis in expressions</source>
+      <translation>Пре отварајуће заграде у изразима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="210"/>
-        <source>After opening parenthesis in expressions</source>
-        <translation>После отварајуће заграде у изразима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="210"/>
+      <source>After opening parenthesis in expressions</source>
+      <translation>После отварајуће заграде у изразима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="220"/>
-        <source>Before closing parenthesis in expressions</source>
-        <translation>Пре затварајуће заграде у изразима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="220"/>
+      <source>Before closing parenthesis in expressions</source>
+      <translation>Пре затварајуће заграде у изразима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="230"/>
-        <source>After closing parenthesis in expressions</source>
-        <translation>После затварајуће заграде у изразима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="230"/>
+      <source>After closing parenthesis in expressions</source>
+      <translation>После затварајуће заграде у изразима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="240"/>
-        <source>After JOIN keywords in FROM clause</source>
-        <translation>После кључних речи JOIN у клаузули FROM</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="240"/>
+      <source>After JOIN keywords in FROM clause</source>
+      <translation>После кључних речи JOIN у клаузули FROM</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="250"/>
-        <source>Put each column constraint in CREATE TABLE into new line</source>
-        <translation>Стави свако ограничење колоне у CREATE TABLE у нови ред</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="250"/>
+      <source>Put each column constraint in CREATE TABLE into new line</source>
+      <translation>Стави свако ограничење колоне у CREATE TABLE у нови ред</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="260"/>
-        <source>After comma</source>
-        <translation>После запете</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="260"/>
+      <source>After comma</source>
+      <translation>После запете</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="270"/>
-        <source>After comma in expressions</source>
-        <translation>После запете у изразима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="270"/>
+      <source>After comma in expressions</source>
+      <translation>После запете у изразима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="280"/>
-        <source>After semicolon</source>
-        <translation>После тачке и запете</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="280"/>
+      <source>After semicolon</source>
+      <translation>После тачке и запете</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="290"/>
-        <location filename="../sqlenterpriseformatter.ui" line="512"/>
-        <source>Never before semicolon</source>
-        <translation>Никада пре тачке и запете</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="290"/>
+      <location filename="../sqlenterpriseformatter.ui" line="512"/>
+      <source>Never before semicolon</source>
+      <translation>Никада пре тачке и запете</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="338"/>
-        <source>White spaces</source>
-        <translation>Размаци</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="338"/>
+      <source>White spaces</source>
+      <translation>Размаци</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="392"/>
-        <source>Before comma in lists</source>
-        <translation>Пре запете у листама</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="392"/>
+      <source>Before comma in lists</source>
+      <translation>Пре запете у листама</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="402"/>
-        <source>After comma in lists</source>
-        <translation>После запете у листама</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="402"/>
+      <source>After comma in lists</source>
+      <translation>После запете у листама</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="412"/>
-        <source>Before opening parenthesis</source>
-        <translation>Пре отварајуће заграде</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="412"/>
+      <source>Before opening parenthesis</source>
+      <translation>Пре отварајуће заграде</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="422"/>
-        <source>After opening parenthesis</source>
-        <translation>После отварајуће заграде</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="422"/>
+      <source>After opening parenthesis</source>
+      <translation>После отварајуће заграде</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="432"/>
-        <source>Before closing parenthesis</source>
-        <translation>Пре затварајуће заграде</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="432"/>
+      <source>Before closing parenthesis</source>
+      <translation>Пре затварајуће заграде</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="442"/>
-        <source>After closing parenthesis</source>
-        <translation>После затварајуће заграде</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="442"/>
+      <source>After closing parenthesis</source>
+      <translation>После затварајуће заграде</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="452"/>
-        <source>No space between SQL function name and opening parenthesis</source>
-        <translation>Без размака између назива SQL функције и отварајуће заграде</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="452"/>
+      <source>No space between SQL function name and opening parenthesis</source>
+      <translation>Без размака између назива SQL функције и отварајуће заграде</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="462"/>
-        <source>Before dot operator (in path to database object)</source>
-        <translation>Пре оператора тачке (у путањи до објекта базе података)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="462"/>
+      <source>Before dot operator (in path to database object)</source>
+      <translation>Пре оператора тачке (у путањи до објекта базе података)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="472"/>
-        <source>After dot operator (in path to database object)</source>
-        <translation>После оператора тачке (у путањи до објекта базе података)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="472"/>
+      <source>After dot operator (in path to database object)</source>
+      <translation>После оператора тачке (у путањи до објекта базе података)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="482"/>
-        <source>Before mathematical operator</source>
-        <translation>Пре математичког оператора</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="482"/>
+      <source>Before mathematical operator</source>
+      <translation>Пре математичког оператора</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="492"/>
-        <source>After mathematical operator</source>
-        <translation>После математичког оператора</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="492"/>
+      <source>After mathematical operator</source>
+      <translation>После математичког оператора</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="502"/>
-        <source>Never before comma</source>
-        <translation>Никада пре запете</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="502"/>
+      <source>Never before comma</source>
+      <translation>Никада пре запете</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="558"/>
-        <source>Names</source>
-        <translation>Имена</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="558"/>
+      <source>Names</source>
+      <translation>Имена</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="583"/>
-        <source>Preferred name wrapper</source>
-        <translation>Жељени омотач имена</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="583"/>
+      <source>Preferred name wrapper</source>
+      <translation>Жељени омотач имена</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="600"/>
-        <source>Always use name wrapping</source>
-        <translation>Увек користи омотавање имена</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="600"/>
+      <source>Always use name wrapping</source>
+      <translation>Увек користи омотавање имена</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="610"/>
-        <source>Uppercase data type names</source>
-        <translation>Називе типова података пиши великим словима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="610"/>
+      <source>Uppercase data type names</source>
+      <translation>Називе типова података пиши великим словима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="620"/>
-        <source>Uppercase keywords</source>
-        <translation>Кључне речи пиши великим словима</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="620"/>
+      <source>Uppercase keywords</source>
+      <translation>Кључне речи пиши великим словима</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="631"/>
-        <source>Comments</source>
-        <translation>Коментари</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="631"/>
+      <source>Comments</source>
+      <translation>Коментари</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="637"/>
-        <source>Preferred comment marker (where possible):</source>
-        <translation>Жељени маркер коментара (где је могуће):</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="637"/>
+      <source>Preferred comment marker (where possible):</source>
+      <translation>Жељени маркер коментара (где је могуће):</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="654"/>
-        <source>Move all comments to the line end</source>
-        <translation>Премести све коментаре на крај реда</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="654"/>
+      <source>Move all comments to the line end</source>
+      <translation>Премести све коментаре на крај реда</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="664"/>
-        <source>Line up comments at the line end</source>
-        <translation>Поравнај коментаре на крају реда</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="664"/>
+      <source>Line up comments at the line end</source>
+      <translation>Поравнај коментаре на крају реда</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="695"/>
-        <source>Preview</source>
-        <translation>Преглед</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="695"/>
+      <source>Preview</source>
+      <translation>Преглед</translation>
     </message>
-</context>
+  </context>
 </TS>
