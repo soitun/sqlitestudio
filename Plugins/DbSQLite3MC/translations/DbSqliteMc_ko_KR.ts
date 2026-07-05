c5e3974141990ef18dfe266cfa63a9f9429646ad
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:54:01 +0200

    New translations dbsqlitemc.ts (Korean)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_ko_KR.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_ko_KR.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ko" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>비밀번호(키)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>비밀번호(키)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>복호화된 데이터베이스를 만들거나 연결하려면 비워 두세요.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>복호화된 데이터베이스를 만들거나 연결하려면 비워 두세요.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>암호화 비밀번호</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>암호화 비밀번호</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>암호화 방식</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>암호화 방식</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>암호화 방식은 데이터베이스를 암호화하는 데 사용되는 암호화 알고리즘을 결정합니다.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>암호화 방식은 데이터베이스를 암호화하는 데 사용되는 암호화 알고리즘을 결정합니다.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>암호화 방식 구성(선택 사항)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>암호화 방식 구성(선택 사항)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>KDF 반복, 레거시 모드 등 SQLite3 Multiple Ciphers 구성을 사용자 지정하기 위한 PRAGMA 문입니다.
+      <translation>KDF 반복, 레거시 모드 등 SQLite3 Multiple Ciphers 구성을 사용자 지정하기 위한 PRAGMA 문입니다.
 데이터베이스를 열 때마다 실행됩니다.
 자세한 내용은 SQLite3 Multiple Ciphers 문서를 참조하세요.</translation>
     </message>
-</context>
+  </context>
 </TS>
