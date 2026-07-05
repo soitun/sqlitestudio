13fa6aaaec9b16584c0c99be3dad79513bea28f2
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:20:31 +0200

    New translations scriptingpython.ts (Dutch)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_nl_NL.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_nl_NL.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Ongeldig gebruik van functie %1. %2 argumenten verwacht, maar %3 gekregen.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Ongeldig gebruik van functie %1. %2 argumenten verwacht, maar %3 gekregen.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Onbekende fout van functie %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Onbekende fout van functie %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Kan de tekenreeksrepresentatie van het Python-object dat als argument aan functie %1 is doorgegeven niet berekenen.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Kan de tekenreeksrepresentatie van het Python-object dat als argument aan functie %1 is doorgegeven niet berekenen.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Kan geen uitvoeringscontext voor functie %1 vinden. Dit is een bug in de Python-plug-in. Rapporteer dit alstublieft.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Kan geen uitvoeringscontext voor functie %1 vinden. Dit is een bug in de Python-plug-in. Rapporteer dit alstublieft.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Fout van Python-functie %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Fout van Python-functie %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Pad naar Python-bibliotheek</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Pad naar Python-bibliotheek</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Kies een Python-bibliotheekbestand</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Kies een Python-bibliotheekbestand</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>De plug-in is niet correct geconfigureerd.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>De plug-in is niet correct geconfigureerd.</translation>
     </message>
-</context>
+  </context>
 </TS>
